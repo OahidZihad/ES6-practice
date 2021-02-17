9327cc29ed0413d5e5f1e06f3a385a9976240295
OahidZihad <oahidzihad1@gmail.com>
2021-02-17 10:12:24 +0600

    truthy and falsy

---
+++ advanced-js/true-false.js
@@ -0,0 +1,20 @@
+// 0
+// ""
+// undefined
+// null
+// NaN
+////////// ei somosto khettre falsy, onno khetre truthy......
+
+const age = 4;
+const name = "Jannati";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+let name1;
+console.log(name1);
