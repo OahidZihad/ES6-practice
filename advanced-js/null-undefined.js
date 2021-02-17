289b6dcaafd307f3782f970ec09be56647613c89
OahidZihad <oahidzihad1@gmail.com>
2021-02-17 10:28:06 +0600

    null vs undefined

---
+++ advanced-js/null-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+
+function add(x,y){
+    console.log(x+y);
+    return 
+}
+
+function add(x,y){
+    console.log(x,y);
+}
+
+const premik = {name: "smart dude", phone: 4874389057};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 8];
+console.log(ages[10]);
