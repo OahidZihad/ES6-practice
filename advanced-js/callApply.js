4bc431b88b22bac34df4411162c646b1edb7d603
OahidZihad <oahidzihad1@gmail.com>
2021-02-25 10:49:44 +0600

    difference between bind, call and apply

--- advanced-js/callApply.js
+++ advanced-js/callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -27,16 +27,30 @@ const heroPerson = {
 const friendlyPerson = {
     firstName: "Hero",
     lastName: "Golam",
-    salary: 20000000
+    salary: 25000
 }
 
 // normalPerson.chargeBill();
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
-heroChargeBill(3000);
-console.log(heroPerson.salary);
-console.log(normalPerson.salary);
 
-const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(1500);
-console.log(friendlyPerson.salary);
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
+// console.log(heroPerson.salary);
+// console.log(normalPerson.salary);
+
+// const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyChargeBill(1500);
+// console.log(friendlyPerson.salary);
+
+
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 10);
+// normalPerson.chargeBill.call(heroPerson, 3000, 300, 30);
+// console.log(heroPerson.salary);
+
+// normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+// console.log(friendlyPerson.salary);
+// console.log(normalPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson, [3000, 300, 30]);
+normalPerson.chargeBill.apply(heroPerson, [4000, 400, 40]);
+console.log(heroPerson.salary);
